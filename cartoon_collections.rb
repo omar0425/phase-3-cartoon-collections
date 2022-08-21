b63fc5bdbb7f56b4e76d67bd5e2ed4689af6c7e0
omar0425 <startfresh87@gmail.com>
2022-08-21 16:57:00 -0400

    commit tree ee82cef7d8c1eed76b69dba2b147a5a85e5f0a13

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+def roll_call_dwarves(array)
+  array.each.with_index(1) do |a,i|
+    puts "#{i}. #{a}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |a|
+  a.capitalize + "!"
+end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(array)
+  array.filter do |a|
+    if a.length >= 4
+      return false
+    else
+      return true 
+end
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+array.find do |a|
+  cheese_types.include?(a)
+
 end
+end
+
+
+# words = ["give", "it", nil, "your", nil, "best", "shot"]
+# words.filter do |word|
+#   if word != nil
+#     word.capitalize
+#   end
+# end
